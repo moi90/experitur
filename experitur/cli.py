1e1556dcd363627bcdf352ebfecdd1199991177e
Simon-Martin Schroeder <sms@informatik.uni-kiel.de>
2019-01-21 16:02:37 +0100

    CLI: clean

--- experitur/cli.py
+++ experitur/cli.py
@@ -16,3 +16,18 @@ def run(experiment_file):
     experiment = Experiment(experiment_file)
 
     experiment.run()
+
+
+@cli.command()
+@click.argument('experiment_file')
+@click.option('--remove-everything', is_flag=True)
+def clean(experiment_file, remove_everything):
+    click.echo('Cleaning failed results from {}...'.format(experiment_file))
+
+    if remove_everything:
+        click.confirm('Do you really want to permanently delete all results of {}?'.format(
+            experiment_file), abort=True)
+
+    experiment = Experiment(experiment_file)
+
+    experiment.clean(remove_everything)
