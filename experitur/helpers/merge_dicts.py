98c602a4a30c45980dacba7b63eb3e2c568cd2cd
Simon-Martin Schroeder <sms@informatik.uni-kiel.de>
2019-01-22 20:37:49 +0100

    helpers.merge_dicts: Overwrite existing keys instead of fail

--- experitur/helpers/merge_dicts.py
+++ experitur/helpers/merge_dicts.py
@@ -7,13 +7,8 @@ def merge_dicts(a, b, path=None):
     if path is None:
         path = []
     for key in b:
-        if key in a:
-            if isinstance(a[key], dict) and isinstance(b[key], dict):
-                merge_dicts(a[key], b[key], path + [str(key)])
-            elif a[key] == b[key]:
-                pass  # same leaf value
-            else:
-                raise Exception('Conflict at %s' % '.'.join(path + [str(key)]))
+        if key in a and isinstance(a[key], dict) and isinstance(b[key], dict):
+            merge_dicts(a[key], b[key], path + [str(key)])
         else:
             a[key] = b[key]
     return a
