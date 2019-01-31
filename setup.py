17aaa9923d8e3d4d48b04d3b09d7e05fbc5620ff
Martin Schröder <Simon-Martin Schrder>
2019-01-31 20:56:14 +0100

    PYPI packaging information

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fp:
+    long_description = fp.read()
+
 setup(
     name='experitur',
     version='0.1',
+    author='Simon-Martin Schroeder',
+    author_email="martin.schroeder@nerdluecht.de",
+    description="Automates machine learning and other computer experiments",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/moi90/experitur",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -14,7 +23,18 @@ setup(
         'sklearn',
         'pandas',
     ],
+    python_requires='>=3.5',
     entry_points={
         'console_scripts': ['experitur=experitur.cli:cli'],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+    ],
 )
