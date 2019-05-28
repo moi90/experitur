e1c26bd95f483581ffa8c963613358eba4e3e1c7
Martin Schröder <Simon-Martin Schrder>
2019-05-28 23:22:47 +0200

    New trial storage and call system.

---
+++ experitur/trial.py
@@ -0,0 +1,305 @@
+import collections.abc
+import glob
+import inspect
+import itertools
+import os.path
+import shutil
+import warnings
+from abc import abstractmethod
+
+import yaml
+
+from experitur.helpers.dumper import ExperiturDumper
+
+
+def _callable_to_name(obj):
+    if callable(obj):
+        return "{}.{}".format(obj.__module__, obj.__name__)
+
+    if isinstance(obj, list):
+        return [_callable_to_name(x) for x in obj]
+
+    if isinstance(obj, dict):
+        return {_callable_to_name(k): _callable_to_name(v) for k, v in obj.items()}
+
+    if isinstance(obj, tuple):
+        return tuple(_callable_to_name(x) for x in obj)
+
+    return obj
+
+
+def _match_parameters(parameters_1, parameters_2):
+    """
+    Decide whether parameters_1 are a subset of parameters_2.
+    """
+
+    # TODO: Ignore _underscore values as these are dynamically calculated
+
+    if set(parameters_1.keys()) <= set(parameters_2.keys()):
+        return all(v == parameters_2[k] for k, v in parameters_1.items())
+
+    return False
+
+
+def _format_independent_parameters(trial_parameters, independent_parameters):
+    if len(independent_parameters) > 0:
+        trial_id = "_".join("{}-{!s}".format(k, trial_parameters[k])
+                            for k in independent_parameters)
+        trial_id = trial_id.replace("/", "_")
+    else:
+        trial_id = "_"
+
+    return trial_id
+
+
+class TrialProxy:
+    """
+    This is the trial object that the experiment interacts with.
+    """
+
+    def __init__(self, trial):
+        self._trial = trial
+
+    def __getattr__(self, name):
+        """
+        Magic attributes.
+        """
+
+        if name in self._trial.data:
+            return self._trial.data[name]
+
+    # see experitur.util
+    def record_defaults(self, prefix, *args, **defaults):
+        """
+        Set default parameters.
+
+        Default parameters can be assigned directly or guessed from a callable.
+        """
+        if len(args) > 1:
+            raise ValueError("Only 1 or 2 positional arguments allowed.")
+
+        parameters = self._trial.data.setdefault("parameters", {})
+
+        # First set explicit defaults
+        for name, value in defaults.items():
+            parameters.setdefault(prefix + name, value)
+
+        if args and callable(args[0]):
+            callable_ = args[0]
+            for param in inspect.signature(callable_).parameters.values():
+                if param.default is not param.empty:
+                    parameters.setdefault(prefix + param.name, param.default)
+
+    def apply(self, prefix, callable_, *args, **kwargs):
+        callable_names = set(
+            param.name
+            for param in inspect.signature(callable_).parameters.values()
+            if param.kind == param.POSITIONAL_OR_KEYWORD)
+
+        start = len(prefix)
+        parameters = {
+            k[start:]: v
+            for k, v in self._trial.data.get("parameters", {}).items()
+            if k.startswith(prefix) and k[start:] in callable_names
+        }
+
+        intersection_names = set(kwargs.keys()) & set(parameters.keys())
+        if intersection_names:
+            warnings.warn("Redefining parameter(s) {} with keyword parameter.".format(
+                ", ".join(intersection_names)))
+
+        for k, v in kwargs.items():
+            parameters[k] = v
+
+        return callable_(*args, **parameters)
+
+    def without_prefix(self, prefix):
+        """
+        Extract parameters beginning with prefix and remove the prefix.
+        """
+        start = len(prefix)
+
+        return {
+            k[start:]: v
+            for k, v in self._trial.data.get("parameters", {}).items()
+            if k.startswith(prefix)
+        }
+
+
+class Trial:
+    """
+    Arguments
+        store: TrialStore
+        callable (optional): Experiment callable
+        data (optional): Trial data
+    """
+
+    def __init__(self, store, callable=None, data=None):
+        self.store = store
+        self.callable = callable
+        self.data = data or {}
+
+    def run(self):
+        self.data["result"] = self.callable(TrialProxy(self))
+
+        return self.data["result"]
+
+    def save(self):
+        self.store[self.data["id"]] = self
+
+
+class TrialStore(collections.abc.MutableMapping):
+    def __init__(self, ctx):
+        self.ctx = ctx
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        pass
+
+    def match(self, callable=None, parameters=None, experiment=None):
+        callable = _callable_to_name(callable)
+
+        result = {}
+        for trial_id, trial in self.items():
+            if callable is not None and _callable_to_name(trial.data.get("callable")) != callable:
+                continue
+
+            if parameters is not None and not _match_parameters(parameters, trial.data.get("parameters", {})):
+                continue
+
+            if experiment is not None and trial.data.get("experiment") != experiment:
+                continue
+
+            result[trial_id] = trial
+
+        return result
+
+    def _make_unique_trial_id(self, experiment_name, trial_parameters, independent_parameters):
+        trial_id = _format_independent_parameters(
+            trial_parameters, independent_parameters)
+
+        trial_id = "{}/{}".format(experiment_name, trial_id)
+
+        try:
+            existing_trial = self[trial_id]
+        except KeyError:
+            # If there is no existing trial with this id, it is unique
+            return trial_id
+
+        # Otherwise, we have to incorporate more independent parameters
+        new_independent_parameters = []
+
+        existing_trial.data.setdefault("parameters", {})
+
+        # Look for parameters in existing_trial that have differing values
+        for name, value in existing_trial.data["parameters"].items():
+            if name in trial_parameters and trial_parameters[name] != value:
+                new_independent_parameters.append(name)
+
+        # Look for parameters that did not exist previously
+        for name in trial_parameters.keys():
+            if name not in existing_trial.data["parameters"]:
+                new_independent_parameters.append(name)
+
+        if new_independent_parameters:
+            # If we found parameters where this trial is different from the existing one, append these to independent
+            independent_parameters.extend(new_independent_parameters)
+            return self._make_unique_trial_id(experiment_name, trial_parameters, independent_parameters)
+
+        # Otherwise, we just append a version number
+        for i in itertools.count():
+            test_trial_id = "{}.{}".format(trial_id, i)
+
+            try:
+                existing_trial = self[test_trial_id]
+            except KeyError:
+                # If there is no existing trial with this id, it is unique
+                return test_trial_id
+
+    def _make_wdir(self, trial_id):
+        wdir = os.path.join(self.ctx.wdir, os.path.normpath(trial_id))
+        os.makedirs(wdir, exist_ok=True)
+        return wdir
+
+    def create(self, parameters, experiment):
+        # Calculate trial_id
+        trial_id = self._make_unique_trial_id(
+            experiment.name,
+            parameters,
+            experiment.independent_parameters)
+
+        wdir = self._make_wdir(trial_id)
+
+        trial_data = {
+            "id": trial_id,
+            "experiment": experiment.name,
+            "parent_experiment": experiment.parent.name if experiment.parent is not None else None,
+            "result": None,
+            "parameters": parameters,
+            "callable": _callable_to_name(experiment.callable),
+            "wdir": wdir,
+        }
+
+        trial = Trial(self, callable=experiment.callable, data=trial_data)
+
+        self[trial_id] = trial
+
+        return trial
+
+
+class FileTrialStore(TrialStore):
+    PATTERN = os.path.join("{}", "trial.yaml")
+    DUMPER = ExperiturDumper
+
+    def __len__(self):
+        return sum(1 for _ in self)
+
+    def __getitem__(self, key):
+        path = os.path.join(self.ctx.wdir, self.PATTERN.format(key))
+        path = os.path.normpath(path)
+
+        try:
+            with open(path) as fp:
+                return Trial(self, data=yaml.load(fp))
+        except FileNotFoundError:
+            raise KeyError
+
+    def __iter__(self):
+        path = os.path.join(self.ctx.wdir, self.PATTERN.format("**"))
+        path = os.path.normpath(path)
+
+        left, right = path.split("**", 1)
+
+        for entry_fn in glob.iglob(path, recursive=True):
+            if os.path.isdir(entry_fn):
+                continue
+
+            # Convert entry_fn back to key
+            k = entry_fn[len(left):-len(right)]
+
+            # Keys use forward slashes
+            k = k.replace("\\", "/")
+
+            yield k
+
+    def __setitem__(self, key, value):
+        path = os.path.join(self.ctx.wdir, self.PATTERN.format(key))
+        path = os.path.normpath(path)
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+
+        with open(path, "w") as fp:
+            yaml.dump(value.data, fp, Dumper=self.DUMPER)
+
+        # raise KeyError
+
+    def __delitem__(self, key):
+        path = os.path.join(self.ctx.wdir, self.PATTERN.format(key))
+
+        try:
+            os.remove(path)
+        except FileNotFoundError:
+            raise KeyError
+
+        shutil.rmtree(os.path.dirname(path))
---
+++ tests/test_trial.py
@@ -0,0 +1,131 @@
+import glob
+import os.path
+import pytest
+
+from experitur.context import Context, push_context
+from experitur.experiment import Experiment
+from experitur.trial import (FileTrialStore, _callable_to_name,
+                             _format_independent_parameters, _match_parameters, Trial)
+
+
+def noop():
+    pass
+
+
+def test__callable_to_name():
+    assert _callable_to_name(noop) == "test_trial.noop"
+    assert _callable_to_name([noop]) == ["test_trial.noop"]
+    assert _callable_to_name((noop,)) == ("test_trial.noop",)
+    assert _callable_to_name({"noop": noop}) == {"noop": "test_trial.noop"}
+
+
+def test__match_parameters():
+    assert _match_parameters({"a": 1, "b": 2}, {"a": 1, "b": 2, "c": 3})
+    assert not _match_parameters({"a": 4, "b": 2}, {"a": 1, "b": 2, "c": 3})
+    assert not _match_parameters({"a": 1, "b": 2, "c": 3}, {"a": 1, "b": 2})
+
+
+def test__format_independent_parameters():
+    parameters = {"a": 1, "b": 2}
+    assert _format_independent_parameters(parameters, []) == "_"
+    assert _format_independent_parameters(parameters, ["a", "b"]) == "a-1_b-2"
+
+
+def test_trial_store(tmp_path):
+    with push_context(Context(str(tmp_path))) as ctx:
+        with FileTrialStore(ctx) as trial_store:
+
+            def test(trial):
+                return {"result": (1, 2)}
+
+            experiment = ctx.experiment("test", parameter_grid={
+                                        "a": [1, 2], "b": [2, 3]})(test)
+
+            def test2(trial):
+                return {"result": (2, 4)}
+
+            experiment2 = ctx.experiment("test2", parent=experiment)(test2)
+
+            parameters = {"a": 1, "b": 2}
+
+            trial_store["foo"] = Trial(trial_store, data={1: "foo", "bar": 2})
+            assert trial_store["foo"].data == {1: "foo", "bar": 2}
+
+            trial_store["bar/baz"] = Trial(trial_store,
+                                           data={1: "foo", "bar": 2})
+            assert trial_store["bar/baz"].data == {1: "foo", "bar": 2}
+
+            trial = trial_store.create(parameters, experiment)
+
+            fake_folder = os.path.join(
+                ctx.wdir,
+                trial_store.PATTERN.format("fake"))
+
+            os.makedirs(fake_folder, exist_ok=True)
+
+            assert trial.data["id"] == "test/a-1_b-2"
+
+            assert "test/a-1_b-2" in trial_store
+
+            assert len(trial_store) == 3
+
+            del trial_store["bar/baz"]
+            del trial_store["foo"]
+
+            with pytest.raises(KeyError):
+                del trial_store["foo"]
+
+            trial_store.create({"a": 2, "b": 3}, experiment)
+            trial_store.create({"a": 3, "b": 4}, experiment)
+            trial_store.create({"a": 3, "b": 4}, experiment2)
+
+            assert set(trial_store.match(callable=test).keys()) == {
+                "test/a-1_b-2", "test/a-2_b-3", "test/a-3_b-4"}
+
+            assert set(trial_store.match(parameters={"a": 1, "b": 2}).keys()) == {
+                "test/a-1_b-2"}
+
+            assert set(trial_store.match(experiment="test2")) == {
+                "test2/a-3_b-4"}
+
+            result = trial.run()
+
+            assert result == {"result": (1, 2)}
+
+            # Write trial data back to the store
+            trial.save()
+
+            assert trial_store["test/a-1_b-2"].data["result"] == {
+                "result": (1, 2)}
+
+            trial_store.create({"a": 1, "b": 2, "c": 1}, experiment)
+
+            experiment.parameter_grid["c"] = [1, 2, 3]
+
+            trial_store.create({"a": 1, "b": 2, "c": 2}, experiment)
+            trial_store.create({"a": 1, "b": 2, "c": 2}, experiment)
+
+
+def test_trial(tmp_path):
+    with push_context(Context(str(tmp_path))) as ctx:
+        with FileTrialStore(ctx) as trial_store:
+
+            def parametrized(a=1, b=2, c=3, d=4):
+                return (a, b, c, d)
+
+            def test(trial):
+                print("trial.wdir:", trial.wdir)
+
+                trial.record_defaults(
+                    "parametrized_", parametrized, d=5)
+
+                return trial.apply("parametrized_", parametrized)
+
+            experiment = ctx.experiment("test", parameter_grid={
+                                        "a": [1, 2], "b": [2, 3]})(test)
+
+            trial = trial_store.create({"a": 1, "b": 2}, experiment)
+
+            result = trial.run()
+
+            assert result == (1, 2, 3, 5)
