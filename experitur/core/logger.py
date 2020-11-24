5e2cdf6f0cc024f87e8cf2db125275bd70bda97a
Simon-Martin Schroeder <sms@informatik.uni-kiel.de>
2020-11-24 15:14:50 +0100

    feat(LoggerBase, YAMLLogger): last_entry, caching

--- experitur/core/logger.py
+++ experitur/core/logger.py
@@ -1,9 +1,11 @@
+import collections.abc
 import os.path
 import weakref
-from abc import abstractmethod
-from typing import TYPE_CHECKING, Any, List, Mapping
-import collections.abc
+from abc import abstractmethod, ABC
+from collections import defaultdict
+from typing import TYPE_CHECKING, Any, Dict, List, Mapping
 
+import cachetools
 import yaml
 
 from experitur.helpers.dumper import ExperiturDumper
@@ -19,12 +21,17 @@ except ImportError:
 LogValues = Mapping[str, Any]
 
 
-class LoggerBase:
-    def __init__(self, trial: "TrialData"):
+class LoggerBase(ABC):
+    def __init__(self, trial: "Trial"):
         self.trial = weakref.proxy(trial)  # Avoid cycles
+        self.last_entry = {}
+
+    def log(self, values: LogValues):
+        self.last_entry.update(values)
+        self._log(values)
 
     @abstractmethod
-    def log(self, values: LogValues):
+    def _log(self, values: LogValues):
         pass
 
     @abstractmethod
@@ -37,14 +44,33 @@ class LoggerBase:
 
         return pd.json_normalize(self.read())
 
+    def aggregate(self, include) -> Dict:
+        include = set(include)
+
+        metrics = defaultdict(list)
+        for entry in self.read():
+            for k, v in entry.items():
+                if k in include:
+                    metrics[k].append(v)
+
+        result = {}
+        for k in metrics:
+            result[f"max_{k}"] = max(metrics[k])
+            result[f"min_{k}"] = min(metrics[k])
+            result[f"final_{k}"] = metrics[k][-1]
+            result[f"mean_{k}"] = metrics[k][-1]
+
 
 class YAMLLogger(LoggerBase):
-    def __init__(self, trial: "TrialData"):
+    # Dict[filename, Tuple[mtime, list]]
+    _cache = cachetools.LRUCache(128)
+
+    def __init__(self, trial: "Trial"):
         super().__init__(trial)
 
         self.log_fn = os.path.join(self.trial.wdir, "log.yaml")
 
-    def log(self, values: Mapping):
+    def _log(self, values: LogValues):
         if not isinstance(values, collections.abc.Mapping):
             raise ValueError(f"Expected mapping, got {values!r}")
 
@@ -52,5 +78,25 @@ class YAMLLogger(LoggerBase):
             yaml.dump(values, fp, Dumper=ExperiturDumper, explicit_start=True)
 
     def read(self):
-        with open(self.log_fn, "r") as fp:
-            return list(yaml.load_all(fp, Loader=yaml.Loader))
+        try:
+            mtime = os.path.getmtime(self.log_fn)
+        except FileNotFoundError:
+            return []
+
+        try:
+            cache_entry = self._cache[self.log_fn]
+        except KeyError:
+            pass
+        else:
+            if cache_entry[0] == mtime:
+                return cache_entry[1]
+
+        try:
+            with open(self.log_fn, "r") as fp:
+                log = list(yaml.load_all(fp, Loader=yaml.Loader))
+        except FileNotFoundError:
+            return []
+
+        self._cache[self.log_fn] = (mtime, log)
+
+        return log
